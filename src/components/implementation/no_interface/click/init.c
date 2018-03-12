0888912e46da1a53658508f833e97061dae696be
Vlad Nitu <vlad.nitu@enseeiht.fr>
2018-03-12 14:19:59 +0100

    * Mark the EOF for the configuration file. Otherwise if the files are linked contiguously the Click parser may parse multiple files. * Added the hypercall to get the conf file index.
    Signed-off-by: Vlad Nitu <vlad.nitu@enseeiht.fr>

--- src/components/implementation/no_interface/click/init.c
+++ src/components/implementation/no_interface/click/init.c
@@ -14,25 +14,39 @@ nfid_t this_nf_id = 0;
  */
 unsigned int conf_file_idx = 0;
 
-#define MAX_CONF_FILES 1
+#define MAX_CONF_FILES 2
 char *conf_files[MAX_CONF_FILES];
 
 static void 
 setup_conf_files(void)
 {
        conf_files[0] = &_binary_conf_file1_start;
+       conf_files[1] = &_binary_conf_file2_start;
+
+       /*
+       * Mark the end of the configuration file.
+       * Without this the Click parser can continue on the 
+       * next configuration file if they are liked contiguously.
+       */
+       *(char *)(&_binary_conf_file1_end - 1) = '\0';
+       *(char *)(&_binary_conf_file2_end - 1) = '\0';
 }
 
 void 
 cos_init(void *args)
 {
+       //unsigned long i;
        struct click_init init_data;
 
        setup_conf_files();
 
+       llboot_comp_confidx_get(&conf_file_idx);
+
        init_data.conf_str = conf_files[conf_file_idx];
        init_data.nf_id = 0;
 
-       printc("cos_init\n");
+       //for (i = &_binary_conf_file1_start; i <= &_binary_conf_file1_end; i++)
+       //       printc("%c", *(char *)i);
+
        click_main(&init_data);
 }
