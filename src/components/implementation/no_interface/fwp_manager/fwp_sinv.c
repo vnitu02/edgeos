c30114823b03399c7f72679012fb429046c9ec65
Vlad Nitu <vlad.nitu@enseeiht.fr>
2018-03-22 19:57:35 +0100

    Separate the sinv/hypercall handlers from the booter.
    Signed-off-by: Vlad Nitu <vlad.nitu@enseeiht.fr>

---
+++ src/components/implementation/no_interface/fwp_manager/fwp_sinv.c
@@ -0,0 +1,119 @@
+#ifndef FWP_SINV_H
+#define FWP_SINV_H
+
+#include <llboot.h>
+
+#include <fwp_manager.h>
+
+struct cos_compinfo *boot_spd_compinfo_get(spdid_t spdid);
+
+static vaddr_t
+boot_comp_malloc(struct click_info *ci, size_t size)
+{
+       /*FIXME to get compinfo from click_info*/
+       struct cos_compinfo *boot_info = boot_spd_compinfo_get(0);
+       struct cos_compinfo *chld_info = cos_compinfo_get(&ci->def_cinfo);
+       vaddr_t vaddr, src_seg, dst_pg, i;
+
+       src_seg = (vaddr_t) cos_page_bump_allocn(boot_info, size);
+       assert(src_seg);
+       vaddr = cos_mem_alias(chld_info, boot_info, src_seg);
+       assert(vaddr);
+
+       for (i = PAGE_SIZE; i < size; i += PAGE_SIZE) {
+              dst_pg = cos_mem_alias(chld_info, boot_info, (src_seg + i));
+              assert(dst_pg);
+       }
+
+       return vaddr;
+}
+
+static int
+boot_comp_confidx_get(unsigned long curr)
+{
+       struct click_info *child_info = (struct click_info *) curr;
+
+       return child_info->conf_file_idx;
+}
+
+/*
+ *  * core function for checkpointing a Click component
+ *   */
+static void
+boot_comp_checkpoint(unsigned long curr, unsigned int nfid)
+{
+       vaddr_t src_seg, addr;
+       size_t vm_range;
+       struct cos_compinfo *parent_cinfo_l = boot_spd_compinfo_get(0);
+       struct click_info *child_info = (struct click_info *) curr;
+
+       /*
+ *        * duplicate memory from this_chld_vm_base to vas_frontier
+ *               */
+       vm_range = parent_cinfo_l->vas_frontier - child_info->booter_vaddr;
+       addr = (vaddr_t) cos_page_bump_allocn(parent_cinfo_l, vm_range);
+       assert(addr);
+
+       memcpy((void *)addr, (void *)child_info->booter_vaddr, vm_range);
+
+       /*
+ *        * The id represents the COMPONENT ID that is checkpointed.
+ *               * Example: template[1] represnts the checkpoint of component 1
+ *                      */
+       templates[nfid].addr = addr;
+       templates[nfid].size = vm_range;
+
+       printc("\tCheckpointing click component's %d data %lx (range:%04x)\n", nfid,
+                     templates[nfid].addr, templates[nfid].size);
+
+       /*
+ *        * switch to the booter component
+ *               */
+       cos_thd_switch(BOOT_CAPTBL_SELF_INITTHD_BASE);
+}
+
+u32_t
+llboot_entry(unsigned long curr, int op, u32_t arg3, u32_t arg4, u32_t *ret2, u32_t *ret3)
+{
+       u32_t ret1 = 0;
+       u32_t error = (1 << 16) - 1;
+
+       switch(op) {
+       case LLBOOT_COMP_MALLOC:
+       {
+              vaddr_t vaddr;
+              vaddr = boot_comp_malloc((struct click_info *)curr, arg3);
+              *ret2 = (u32_t)vaddr;
+
+              break;
+       }
+       case LLBOOT_COMP_CONFIDX_GET:
+       {
+              int conf_file_idx;
+              conf_file_idx = boot_comp_confidx_get(curr);
+              *ret2 = (u32_t)conf_file_idx;
+
+              break;
+       }
+       case LLBOOT_COMP_CHECKPOINT:
+       {
+              boot_comp_checkpoint(curr, arg3);
+
+              break;
+       }
+       case LLBOOT_COMP_EMPTY:
+       {
+              cos_thd_switch(BOOT_CAPTBL_SELF_INITTHD_BASE);
+              break;
+       }
+       default:
+       {
+              assert(0);
+
+              ret1 = error;
+       }
+       }
+
+       return ret1;
+}
+#endif /*FWP_MANAGER_H*/
