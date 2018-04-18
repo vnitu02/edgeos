fdb6db9d5da77ff160eb61a78aaeecd5eb404de4
Vlad Nitu <vlad.nitu@enseeiht.fr>
2018-04-18 20:11:12 +0200

    Restore eos_ring_init to the initial implementation.

--- src/components/implementation/no_interface/fwp_manager/eos_ring.h
+++ src/components/implementation/no_interface/fwp_manager/eos_ring.h
@@ -56,26 +56,24 @@ get_output_ring(void *rh){
 
 /*We need both addresses in order to corectly page align*/
 static inline void
-eos_rings_init(void *booter_rh, void *comp_rh)
+eos_rings_init(void *rh)
 {
        struct eos_ring *input_ring, *output_ring;
 	char *pkts, *end_of_rings;
 	int i;
-       unsigned long offset = ((unsigned long)comp_rh) - ((unsigned long)booter_rh);
 
-	assert(((unsigned long)booter_rh & (~PAGE_MASK)) == 0);
-	assert(((unsigned long)comp_rh & (~PAGE_MASK)) == 0);
-	assert(offset > 0);
-       memset(booter_rh, 0, sizeof(struct eos_ring));
+	assert(((unsigned long)rh & (~PAGE_MASK)) == 0);
 
-       input_ring = (struct eos_ring *)booter_rh;
+       memset(rh, 0, sizeof(struct eos_ring));
+
+       input_ring = (struct eos_ring *)rh;
        input_ring->ring = (struct eos_ring_node *)((char *)input_ring + sizeof(struct eos_ring));
 
        output_ring = (struct eos_ring *)((char *)input_ring->ring + EOS_RING_SIZE * sizeof(struct eos_ring_node));
        output_ring->ring = (struct eos_ring_node *)((char *)output_ring + sizeof(struct eos_ring));
 
        end_of_rings = (char *)output_ring->ring + EOS_RING_SIZE * sizeof(struct eos_ring_node);
-       pkts = (char *)round_up_to_page(end_of_rings) + offset;
+       pkts = (char *)round_up_to_page(end_of_rings);
 
        for(i=0; i<EOS_RING_SIZE; i++) {
               output_ring->ring[i].state = PKT_EMPTY;
@@ -85,9 +83,6 @@ eos_rings_init(void *booter_rh, void *comp_rh)
 		input_ring->ring[i].pkt = pkts;
 		pkts += EOS_PKT_MAX_SZ;
 	}
-
-       input_ring->ring = (struct eos_ring_node *)((char *)input_ring->ring + offset);
-       output_ring->ring = (struct eos_ring_node *)((char *)output_ring->ring + offset);
 }
 
 #endif /* __ESO_RING_H__ */
